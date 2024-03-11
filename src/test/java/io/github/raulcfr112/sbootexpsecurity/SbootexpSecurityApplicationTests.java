b5811817c712e165ce8170abbfcd722f04b2a599
Raul <raul.cfr112@gmail.com>
2024-03-11 09:31:38 -0400

    Implementado credenciais de acesso em memória para testes

---
+++ src/test/java/io/github/raulcfr112/sbootexpsecurity/SbootexpSecurityApplicationTests.java
@@ -0,0 +1,13 @@
+package io.github.raulcfr112.sbootexpsecurity;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class SbootexpSecurityApplicationTests {
+
+    @Test
+    void contextLoads() {
+    }
+
+}
